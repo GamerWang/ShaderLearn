5e8b3ab3b522514fe06210be681b5bf09984a63d
GamerWang <GamerWang@outlook.com>
2018-05-11 14:54:32 +0800

    Learn Chapter 10
    Learn Chapter 10_1
    
    Add Reflection shader
    
    Add Refraction shader
    
    Add Fresnel shader
    
    Learn Chapter 10_2
    
    learn Target Texture
    
    learn GrabPass

---
+++ Assets/_Chapters/_Chapter10/10_1/Editor/RenderCubemapWizard.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class RenderCubemapWizard : ScriptableWizard {
+	public Transform _renderFromPosition;
+	public Cubemap _cubemap;
+
+	void OnWizardUpdate() {
+		helpString = "Select transform to render from and cubemap to render into";
+		isValid = (_renderFromPosition != null) && (_cubemap != null);
+	}
+
+	void OnWizardCreate() {
+		// create temporary camera for rendering
+		GameObject tempCamera = new GameObject("CubemapCamera");
+		var camera = tempCamera.AddComponent<Camera>();
+		// place it on the object's position
+		tempCamera.transform.position = _renderFromPosition.position;
+		// render into cubemap
+		camera.RenderToCubemap(_cubemap);
+		// destroy temporary camera
+		DestroyImmediate(tempCamera);
+	}
+
+	[MenuItem("GameObject/Render into Cubemap")]
+	static void RenderCubemap() {
+		ScriptableWizard.DisplayWizard<RenderCubemapWizard>(
+			"Render cubemap", "Render!");
+	}
+}
---
+++ Assets/_Chapters/_Chapter10/10_2/10_2_1/Chapter10_2_Monitor.shader
@@ -0,0 +1,50 @@
+﻿Shader "_Unity Shaders Book/Chapter 10/Chapter10_2_Monitor"
+{
+	Properties
+	{
+		_MainTex("Main Tex", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" "Queue"="Geometry" }
+
+		Pass
+		{
+			CGPROGRAM
+
+			#pragma vertex vert
+			#pragma fragment frag
+
+			sampler2D _MainTex;
+
+			struct VertexInput {
+				float4 vertex : POSITION;
+				float3 texcoord : TEXCOORD0;
+			};
+
+			struct VertexOutput {
+				float4 pos : SV_POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			VertexOutput vert(VertexInput v) {
+				VertexOutput o;
+
+				o.pos = UnityObjectToClipPos(v.vertex);
+				o.uv = v.texcoord;
+				// Mirror needs to flip x
+				//o.uv.x = 1 - o.uv.x;
+
+				return o;
+			}
+
+			fixed4 frag(VertexOutput i) : SV_Target {
+				return tex2D(_MainTex, i.uv);
+			}
+
+			ENDCG
+		}
+	}
+
+	FallBack "Reflective/VertexLit"
+}
